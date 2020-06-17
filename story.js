5bd274764634bffed94eabb8adf308a7fbcfaf3d
dalal almasoud <dalal.alamsoud@gmail.com>
2020-06-17 13:27:08 +0300

    splved variables task

--- story.js
+++ story.js
@@ -1,7 +1,11 @@
 // Define the name here on line 2
-
+const name = "Dalal";
 // Define the age here on line 4
-
+const age = 41;
 // Define the hobby here on line 6
-
+const hobby = "Horse Ridding";
 // Print the story here on line 8
+console.log(`Mr./Mrs. ${name}, in 4 years, you will be ${
+  age + 4
+}. By then you'll have decades of ${hobby} practice!
+`);
